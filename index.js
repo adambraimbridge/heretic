b0ca4c9c57e75c457731b94f079aefeb6a05e1bc
Adam Braimbridge <adam.braimbridge@ft.com>
2019-06-13 08:30:17 +0100

    Export a handler method

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-debugger;
-console.log(process)
+exports.handler = async (event, context) => {
+    if (event.httpMethod !== "POST") {
+        return { statusCode: 405, body: "Method Not Allowed" }
+    }
+    const params = querystring.parse(event.body)
+    const message = params.message || ''
+    return {
+        statusCode: 200,
+        body: `🕊 Please think twice before using @here. ${message}`
+    }
+}
