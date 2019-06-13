e25973317f1325b31119b016556abfc6c7a028c8
Adam Braimbridge <adam.braimbridge@ft.com>
2019-06-13 09:03:39 +0100

    MVP is now working

--- index.js
+++ index.js
@@ -3,17 +3,13 @@ exports.handler = async (event, context) => {
     if (event.httpMethod !== 'POST') {
         return { statusCode: 405, body: 'Method Not Allowed' }
     }
-    
-    console.log(querystring.parse(event.body))
-    
     const params = querystring.parse(event.body)
     const { SLACK_TOKEN } = process.env
     if (!params.token || params.token !== SLACK_TOKEN) {
         return { statusCode: 401, body: 'Unauthorized' }
     }
-    const message = params.message || ''
     return {
         statusCode: 200,
-        body: `🕊 Please think twice before using @here. ${message}`
+        body: `🕊 Please think twice before using @here. ${params.text || ''}`
     }
 }
