43820a7e87e6e55ef3629b7940500fa1598a8cdd
Adam Braimbridge <adam.braimbridge@ft.com>
2019-06-13 09:39:32 +0100

    Using the 'in_channel' response type forces both the issued slash command and the slash command response to be shown to all users within the channel/DM.

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const querystring = require ('querystring')
-exports.handler = async (event, context) => {
-    if (event.httpMethod !== 'POST') {
+const fetch = require('node-fetch')
+exports.handler = async ({httpMethod, body}) => {
+    if (httpMethod !== 'POST') {
         return { statusCode: 405, body: 'Method Not Allowed' }
     }
-    const params = querystring.parse(event.body)
-    const { SLACK_TOKEN } = process.env
+    const params = querystring.parse(body)
+    const { SLACK_TOKEN, SLACK_WEBHOOK_URL } = process.env
     if (!params.token || params.token !== SLACK_TOKEN) {
         return { statusCode: 401, body: 'Unauthorized' }
     }
-    return {
-        statusCode: 200,
+    return fetch(process.env.SLACK_WEBHOOK_URL, {
         headers: {
-            'content-type': 'application/json'
+            "content-type": "application/json"
         },
-        body: `{
-            'response_type': 'in_channel',
-            'text': '🕊 Please think twice before using @here. ${params.text || ""}'
-        }`
-    }
+        method: "POST",
+        body: JSON.stringify({
+            'text': `🕊 Please think twice before using @here. ${params.text || ''}`
+        })
+        .then(() => ({
+            statusCode: 200,
+        }))
+        .catch(error => ({
+            statusCode: 422,
+            body: `Oops! Something went wrong. ${error}`
+        }))
+    })
 }
